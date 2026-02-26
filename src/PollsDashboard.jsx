31931f2d3f05588868d89189323a0a4a992fc10d
Oluwamayowa <ademayo234@gmail.com>
2026-02-26 13:43:11 -0800

    Worked on the polls section in the dashboard

--- src/PollsDashboard.jsx
+++ src/PollsDashboard.jsx
@@ -80,59 +80,118 @@ const PollsDashboard = ({ setactiveTab }) => {
                 <p className="text-xl">Trending Now</p>
             </div>
             <div>
-                <div className='w-96'>
-                    <div className='grid grid-cols-[0.1fr_1fr] gap-x-2 items-center'>
-                        <div>
-                            <img src={ProfileOne} alt="" className='h-8 w-8 rounded-full object-cover' />
-                        </div>
-                        <div className='grid grid-cols-[1fr_0.3fr] items-center'>
+                <div className='w-fit rounded-lg bg-white shadow-md p-6'>
+                    <div className=''>
+                        <div className='grid grid-cols-[0.1fr_1fr] gap-x-2 items-center'>
                             <div>
-                                <p className='text-extrabold'>Gun Jeum Di</p>
-                                <p className='text-[14px] text-gray-600 font raleway'>Posted {PollInitTime} hrs ago</p>
+                                <img src={ProfileOne} alt="" className='h-8 w-8 rounded-full object-cover' />
                             </div>
-                            <div className='h-7 w-13 bg-red-200 flex items-center justify-center rounded-md'>
-                                <p className='font-raleway text-red-600'>HOT</p>
+                            <div className='flex justify-between items-center '>
+                                <div>
+                                    <p className='text-extrabold'>Gun Jeum Di</p>
+                                    <p className='text-[14px] text-gray-600 font raleway'>Posted {PollInitTime} hrs ago</p>
+                                </div>
+                                <div className='h-7 w-13 bg-red-200 flex items-center justify-center rounded-md'>
+                                    <p className='font-raleway text-red-600'>HOT</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div>
-                    <p>Who should win Content Creator of the Year?</p>
-                </div>
-                <div className='grid gap-y-3'>
-                    <div className='w-80 h-10 bg-blue-50 flex items-center justify-between rounded-md'>
-                        <div className='w-54 h-10 bg-blue-100 flex items-center rounded-md'>
-                            <p className='ml-2'>Ishowspeed</p>
-                        </div>
-                        <p className='mr-2 font-sans text-blue-500'>{percentage}%</p>
+                    <div>
+                        <p className='my-4'>Who should win Content Creator of the Year?</p>
                     </div>
-                    <div className='w-80 h-10 bg-blue-50 flex items-center justify-between rounded-md'>
-                        <div className='w-20 h-10 bg-blue-100 flex items-center rounded-md'>
-                            <p className='ml-2'>Mirabel</p>
+                    <div className='grid gap-y-3'>
+                        <div className='w-100 h-10 bg-blue-50 flex items-center justify-between rounded-md'>
+                            <div className='w-65 h-10 bg-blue-100 flex items-center rounded-md'>
+                                <p className='ml-2'>Ishowspeed</p>
+                            </div>
+                            <p className='mr-2 font-sans text-blue-500'>{percentage}%</p>
                         </div>
-                        <p className='mr-2 font-sans text-blue-500'>{25}%</p>
+                        <div className='w-100 h-10 bg-blue-50 flex items-center justify-between rounded-md'>
+                            <div className='w-25 h-10 bg-blue-100 flex items-center rounded-md'>
+                                <p className='ml-2'>Mirabel</p>
+                            </div>
+                            <p className='mr-2 font-sans text-blue-500'>{25}%</p>
+                        </div>
+                        <div className='w-100 h-10 bg-blue-50 flex items-center justify-between rounded-md'>
+                            <div className='w-10 h-10 bg-blue-100 flex items-center rounded-md'>
+                                <p className='ml-2'>Simi</p>
+                            </div>
+                            <p className='mr-2 font-sans text-blue-500'>{10}%</p>
+                        </div>
                     </div>
-                    <div className='w-80 h-10 bg-blue-50 flex items-center justify-between rounded-md'>
-                        <div className='w-8 h-10 bg-blue-100 flex items-center rounded-md'>
-                            <p className='ml-2'>Ishowspeed</p>
-                        </div>
-                        <p className='mr-2 font-sans text-blue-500'>{10}%</p>
+                    <hr className='my-4 text-gray-400' />
+                    <div className='text-gray-500 flex justify-between '>
+                        <span className='flex items-center'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-4 w-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                            </svg>
+                            <p className='font-raleway'>2,435 voted</p>
+                        </span>
+                        <span className='flex items-center'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-4 w-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                            <p className='font-raleway'>Ends in 3h</p>
+                        </span>
                     </div>
                 </div>
-                <hr className='w-80 my-4 text-gray-400' />
-                <div>
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
-                        </svg>
-                        <p>2,435 voted</p>
-                    </span>
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                        </svg>
-                        <p>Ends in 3h</p>
-                    </span>
+                
+                <div className='w-fit rounded-lg bg-white shadow-md p-6'>
+                    <div className=''>
+                        <div className='grid grid-cols-[0.1fr_1fr] gap-x-2 items-center'>
+                            <div>
+                                <img src={ProfileOne} alt="" className='h-8 w-8 rounded-full object-cover' />
+                            </div>
+                            <div className='flex justify-between items-center '>
+                                <div>
+                                    <p className='text-extrabold'>Gun Jeum Di</p>
+                                    <p className='text-[14px] text-gray-600 font raleway'>Posted {PollInitTime} hrs ago</p>
+                                </div>
+                                <div className='h-7 w-13 bg-red-200 flex items-center justify-center rounded-md'>
+                                    <p className='font-raleway text-red-600'>HOT</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p className='my-4'>Anime of the Year????</p>
+                    </div>
+                    <div className='grid gap-y-3'>
+                        <div className='w-100 h-10 bg-blue-50 flex items-center justify-between rounded-md'>
+                            <div className='w-65 h-10 bg-blue-100 flex items-center rounded-md'>
+                                <p className='ml-2'>Ishowspeed</p>
+                            </div>
+                            <p className='mr-2 font-sans text-blue-500'>{percentage}%</p>
+                        </div>
+                        <div className='w-100 h-10 bg-blue-50 flex items-center justify-between rounded-md'>
+                            <div className='w-25 h-10 bg-blue-100 flex items-center rounded-md'>
+                                <p className='ml-2'>Mirabel</p>
+                            </div>
+                            <p className='mr-2 font-sans text-blue-500'>{25}%</p>
+                        </div>
+                        <div className='w-100 h-10 bg-blue-50 flex items-center justify-between rounded-md'>
+                            <div className='w-10 h-10 bg-blue-100 flex items-center rounded-md'>
+                                <p className='ml-2'>Simi</p>
+                            </div>
+                            <p className='mr-2 font-sans text-blue-500'>{10}%</p>
+                        </div>
+                    </div>
+                    <hr className='my-4 text-gray-400' />
+                    <div className='text-gray-500 flex justify-between '>
+                        <span className='flex items-center'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-4 w-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                            </svg>
+                            <p className='font-raleway'>2,435 voted</p>
+                        </span>
+                        <span className='flex items-center'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-4 w-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                            <p className='font-raleway'>Ends in 3h</p>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
