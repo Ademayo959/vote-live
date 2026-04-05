bc20d05d6a5fe9135cd2a8e164068c7135c16008
Oluwamayowa <ademayo234@gmail.com>
2026-04-05 16:59:41 -0700

    Worked on the responsiveness of the auth page

--- src/Authpage.jsx
+++ src/Authpage.jsx
@@ -8,7 +8,7 @@ const Authpage = () => {
     const [clickedTwo, setClickedTwo] = useState(false);
     return (
         <div className="auth-page flex font-montserrat">
-            <div className="bg-linear-to-br from-[#1e40a9] to-[#1e2b42] w-[50%] h-screen text-white py-6 px-8">
+            <div className="bg-linear-to-br from-[#1e40a9] to-[#1e2b42] w-[50%] h-screen text-white py-6 px-8 max-sm:hidden">
                 <p className="text-4xl mb-4 mt-66">Your voice matters in building a better campus.</p>
                 <p className="font-raleway">Join thousands of students securely casting their votes for student union leaders, faculty representatives, and campus initiatives.</p>
                 <div className="px-4 py-8 my-8 grid gap-4 border border-gray-50/10 rounded-lg bg-white/10 backdrop-blur-md">
@@ -26,13 +26,13 @@ const Authpage = () => {
                     </div>
                 </div>
             </div>
-            <div className='bg-accent-blue flex w-[50%]'>
+            <div className='bg-accent-blue flex w-[50%] max-sm:w-full'>
                 <div className='mx-auto'>
                     <div className='mb-8'>
                         <p className='text-2xl font-extrabold mt-4 mb-2 text-center'>Create an account</p>
                         <p className='text-gray-500 text-center text-[14px] font-raleway'>Enter your details below to get started</p>
                     </div>
-                    <div className='w-96 h-12 bg-soft-blue flex items-center justify-center gap-2 rounded-md mb-6'>
+                    <div className='w-96 h-12 bg-soft-blue flex items-center justify-center gap-2 rounded-md mb-6 max-sm:w-[90%] max-sm:justify-self-center'>
                         <div className={`flex items-center justify-center w-46 h-10 rounded-md ${clickedOne ? "bg-white shadow-sm" : "bg-transparent"}`} onClick={() => { setactiveMenu("signup"); setClickedOne(true) }}>
                             <p>Sign Up</p>
                         </div>
@@ -40,13 +40,13 @@ const Authpage = () => {
                             <p>Log In</p>
                         </div>
                     </div>
-                    <div>
+                    <div className='max-sm:justify-self-center'>
                         {activeMenu === "signup" &&
                             <div>
                                 <div>
                                     <div>
                                         <p>Full Name</p>
-                                        <div className='flex items-center gap-2 border border-gray-300 px-2 py-1 bg-white rounded-md my-2 w-96'>
+                                        <div className='flex items-center gap-2 border border-gray-300 px-2 py-1 bg-white rounded-md my-2 w-96 max-sm:w-[93%]'>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 text-gray-600">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                                             </svg>
@@ -55,7 +55,7 @@ const Authpage = () => {
                                     </div>
                                     <div>
                                         <p>Matric Number</p>
-                                        <div className='flex items-center gap-2 border border-gray-300 px-2 py-1 bg-white rounded-md my-2 w-96'>
+                                        <div className='flex items-center gap-2 border border-gray-300 px-2 py-1 bg-white rounded-md my-2 w-96 max-sm:w-[93%]'>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 text-gray-600">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
                                             </svg>
@@ -64,7 +64,7 @@ const Authpage = () => {
                                     </div>
                                     <div>
                                         <p>Phone Number</p>
-                                        <div className='flex items-center gap-2 border border-gray-300 px-2 py-1 bg-white rounded-md my-2 w-96'>
+                                        <div className='flex items-center gap-2 border border-gray-300 px-2 py-1 bg-white rounded-md my-2 w-96 max-sm:w-[93%]'>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 text-gray-600">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
                                             </svg>
@@ -73,7 +73,7 @@ const Authpage = () => {
                                     </div>
                                     <div>
                                         <p>Password</p>
-                                        <div className='flex items-center gap-2 border border-gray-300 px-2 py-1 bg-white rounded-md my-2 w-96'>
+                                        <div className='flex items-center gap-2 border border-gray-300 px-2 py-1 bg-white rounded-md my-2 w-96 max-sm:w-[93%]'>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 text-gray-600">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                                             </svg>
@@ -86,7 +86,7 @@ const Authpage = () => {
                                         <p className='text-[12px] text-gray-600 text-center'><input type="checkbox" /> By clicking create account, I agree that I have read and <br /> accepted the <a href="#" className='text-blue-500 underline'>Terms of Use</a> and <a href="#" className='text-blue-500 underline'>Privacy Policy</a></p>
                                     </div>
                                     <div>
-                                        <div className='bg-blue-500 flex gap-2 text-white w-96 h-10 items-center justify-center rounded-md hover:gap-4 transition-all'>
+                                        <div className='bg-blue-500 flex gap-2 text-white w-96 h-10 items-center justify-center rounded-md hover:gap-4 transition-all max-sm:w-[93%]'>
                                             <p>Create Account</p>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
@@ -94,7 +94,7 @@ const Authpage = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className='text-center flex items-center justify-between'>
+                                <div className='text-center flex items-center justify-between max-sm:w-[93%]'>
                                     <p className='text-[12px] text-gray-500'>Having Trouble? <a href="#" className='text-blue-500 underline'>Contact Support</a></p>
                                     <Link to="/">
                                     <p className='text-[13px] text-blue-500 font-raleway'>Back to Home</p>
@@ -107,7 +107,7 @@ const Authpage = () => {
                                 <div className='mt-10'>
                                     <div>
                                         <p>Matric Number</p>
-                                        <div className='flex items-center gap-2 border border-gray-300 px-2 py-1 bg-white rounded-md my-2 w-96'>
+                                        <div className='flex items-center gap-2 border border-gray-300 px-2 py-1 bg-white rounded-md my-2 w-96 max-sm:w-[93%]'>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 text-gray-600">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
                                             </svg>
@@ -116,7 +116,7 @@ const Authpage = () => {
                                     </div>
                                     <div>
                                         <p>Password</p>
-                                        <div className='flex items-center gap-2 border border-gray-300 px-2 py-1 bg-white rounded-md my-2 w-96'>
+                                        <div className='flex items-center gap-2 border border-gray-300 px-2 py-1 bg-white rounded-md my-2 w-96 max-sm:w-[93%]'>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 text-gray-600">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                                             </svg>
@@ -126,7 +126,7 @@ const Authpage = () => {
                                 </div>
                                 <div className='grid gap-4 my-4'>
                                     <div>
-                                        <div className='bg-blue-500 flex gap-2 text-white w-96 h-10 items-center justify-center rounded-md hover:gap-4 transition-all'>
+                                        <div className='bg-blue-500 flex gap-2 text-white w-96 h-10 items-center justify-center rounded-md hover:gap-4 transition-all max-sm:w-[93%]'>
                                             <p>Log In</p>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
@@ -134,7 +134,7 @@ const Authpage = () => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className='text-center flex items-center justify-between'>
+                                <div className='text-center flex items-center justify-between max-sm:w-[93%]'>
                                     <p className='text-[12px] text-gray-500'>Having Trouble? <a href="#" className='text-blue-500 underline'>Contact Support</a></p>
                                     <Link to="/">
                                         <p className='text-[13px] text-blue-500 font-raleway'>Back to Home</p>
