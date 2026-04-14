8449a539b10fe40644a49328152121e1046d355e
Oluwamayowa <ademayo234@gmail.com>
2026-04-13 18:37:05 -0700

    Fixed the responsiveness bug in the landing page

--- src/Home.jsx
+++ src/Home.jsx
@@ -175,20 +175,20 @@ const Home = () => {
                                 </div>
                             </div>
                             <div className="mb-0 mt-0">
-                                <h1 className="text-xl font-extrabold">Engineering Faculty President</h1>
+                                <h1 className="text-xl font-extrabold max-sm:text-[4.75vw]">Engineering Faculty President</h1>
                             </div>
                             <div className="flex gap-2 items-center">
                                 <div className="flex items-center text-gray-500 gap-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 max-sm:w-4 max-sm:h-4">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                                     </svg>
-                                    <p className="text-sm">2.4k Voted</p>
+                                    <p className="text-sm max-sm:text-[3vw]">2.4k Voted</p>
                                 </div>
                                 <div className="flex items-center text-gray-500 gap-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 max-sm:w-4 max-sm:h-4">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                     </svg>
-                                    <p className="text-sm">Ends in 2h</p>
+                                    <p className="text-sm max-sm:text-[3vw]">Ends in 2h</p>
                                 </div>
                             </div>
                             <div className="grid grid-rows-2 gap-0">
@@ -226,20 +226,20 @@ const Home = () => {
                                 </div>
                             </div>
                             <div className="mb-0 mt-0">
-                                <h1 className="text-xl font-extrabold">Best Campus Football Team</h1>
+                                <h1 className="text-xl font-extrabold max-sm:text-[4.75vw]">Best Campus Football Team</h1>
                             </div>
                             <div className="flex gap-2 items-center">
                                 <div className="flex items-center text-gray-500 gap-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 max-sm:w-4 max-sm:h-4">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                                     </svg>
-                                    <p className="text-sm">11.7k Voted</p>
+                                    <p className="text-sm max-sm:text-[3vw]">11.7k Voted</p>
                                 </div>
                                 <div className="flex items-center text-gray-500 gap-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 max-sm:w-4 max-sm:h-4">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                     </svg>
-                                    <p className="text-sm">4 Days Left</p>
+                                    <p className="text-sm max-sm:text-[3vw]">4 Days Left</p>
                                 </div>
                             </div>
                             <div className="grid mt-4">
@@ -270,20 +270,20 @@ const Home = () => {
                                 </div>
                             </div>
                             <div className="mb-0 mt-0">
-                                <h1 className="text-xl font-extrabold">Computer Science Dept. Sec</h1>
+                                <h1 className="text-xl font-extrabold max-sm:text-[4.75vw]">Computer Science Dept. Sec</h1>
                             </div>
                             <div className="flex gap-2 items-center">
                                 <div className="flex items-center text-gray-500 gap-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 max-sm:w-4 max-sm:h-4">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                                     </svg>
-                                    <p className="text-sm">892 Voted</p>
+                                    <p className="text-sm max-sm:text-[3vw]">892 Voted</p>
                                 </div>
                                 <div className="flex items-center text-gray-500 gap-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 max-sm:w-4 max-sm:h-4">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                     </svg>
-                                    <p className="text-sm">Ends in 45m</p>
+                                    <p className="text-sm max-sm:text-[3vw]">Ends in 45m</p>
                                 </div>
                             </div>
                             <div className="grid grid-rows-2 gap-0">
