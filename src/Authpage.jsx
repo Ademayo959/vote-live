9925543d50c376fec17bfab2df7882cdad7175ae
Oluwamayowa <ademayo234@gmail.com>
2026-04-15 15:42:30 -0700

    fixed a bug in auth page

--- src/Authpage.jsx
+++ src/Authpage.jsx
@@ -201,7 +201,7 @@ const Authpage = () => {
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 text-gray-600">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                                             </svg>
-                                            <input name='loginPassword' onChange={(e) => { setloginPassword(e.target.value) }} type={showPassword ? "password" : "text"} placeholder='Create your Password' className='h-8 w-full outline-0 placeholder:text-[14px] placeholder:font-raleway' />
+                                            <input name='loginPassword' onChange={(e) => { setloginPassword(e.target.value) }} type={showPassword ? "password" : "text"} placeholder='Enter your Password' className='h-8 w-full outline-0 placeholder:text-[14px] placeholder:font-raleway' />
                                             <div>
                                                 {showPassword ?
                                                     <svg onClick={() => { setshowPassword(false) }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5 text-gray-600">
