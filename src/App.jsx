eaf8d8edf9d0ae82c655729a137b9209105187f2
Oluwamayowa <ademayo234@gmail.com>
2026-04-15 15:20:00 -0700

    added protected routing

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,14 @@ import CreateElectionModal from './CreateElectionModal';
 import CreatePollModal from './CreatePollModal';
 import HowItWorks from './HowItWorks';
 import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
      <Route path='/help' element={<HelpAndSupport />}/>
-      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/dashboard' element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
       <Route path="/signup" element={<Authpage />} />
       <Route path="/login" element={<Authpage />} />
       <Route path='/howitworks' element={<HowItWorks />} />
---
+++ src/ProtectedRoute.jsx
@@ -0,0 +1,31 @@
+import { useState,useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/firebase";
+
+
+const ProtectedRoute = ({children}) => {
+    const [currentUser, setcurrentUser] = useState(undefined)
+
+    useEffect(() => {
+        const unsucscribe = onAuthStateChanged(auth, (user) => {
+            setcurrentUser(user)
+        })
+
+        return () => unsucscribe()
+    }, [])
+
+    if (currentUser === undefined) {
+        return (
+            <div>
+                <p>Loading....</p>
+            </div>
+        )
+    } else if (currentUser === null) {
+       return <Navigate to="/login" />
+    }
+
+    return children
+}
+ 
+export default ProtectedRoute;
