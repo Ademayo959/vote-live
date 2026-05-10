5dfadf41d488d0129058b7b07bf248763c231009
Oluwamayowa <ademayo234@gmail.com>
2026-05-09 21:48:42 -0700

    removed the two filters button

--- src/ElectionsDashboard.jsx
+++ src/ElectionsDashboard.jsx
@@ -92,18 +92,6 @@ const ElectionsDashboard = ({ setactiveTab }) => {
                         <input type="text" className="outline-none w-[99%] font-raleway placeholder:text-raleway placeholder:text-sm" placeholder="Search by Election Name, ID or Department...." />
                     </div>
                     <div className="flex gap-6 max-sm:gap-3">
-                        <div className="flex gap-2 w-24 border bg-white border-gray-300 items-center justify-center text-gray-600 rounded-md max-sm:p-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
-                            </svg>
-                            <p>Filters</p>
-                        </div>
-                        <div className="flex gap-2 w-24 border bg-white border-gray-300 items-center justify-center text-gray-600 rounded-md max-sm:p-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5" />
-                            </svg>
-                            <p>Sort</p>
-                        </div>
                     </div>
                 </div>
                 <div className="my-8 grid grid-cols-3 gap-y-6 gap-x-6 justify-self-center max-[840px]:grid-cols-2 max-sm:w-full max-sm:grid-cols-1">
