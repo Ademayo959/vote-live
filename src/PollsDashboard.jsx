de46b0634d979a6ca654e5fd57b670d0684e7d85
Oluwamayowa <ademayo234@gmail.com>
2026-06-05 19:06:10 -0700

    fixed the styling issue on the polls card

--- src/PollsDashboard.jsx
+++ src/PollsDashboard.jsx
@@ -195,18 +195,18 @@ const PollsDashboard = ({ setactiveTab, userName }) => {
                         </div>
                         <hr className='my-4 text-gray-400' />
                         <div className='text-gray-500 flex justify-between '>
-                            <div className="flex items-center gap-4">
-                                <span className='flex items-center'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-4 w-5">
+                            <div className="flex items-center gap-4 max-sm:gap-1 max-sm:text-[13px]">
+                                <span className='flex items-center gap-1'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-4 w-4">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                                     </svg>
                                     <p className='font-raleway'>{poll.totalVotes} voted</p>
                                 </span>
-                                <span className='flex items-center'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-4 w-5">
+                                <span className='flex items-center gap-1'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-4 w-4">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                     </svg>
-                                    <p className='font-raleway text-[15px]'>{poll.createdAt.toDate().toLocaleDateString()}</p>
+                                    <p className='font-raleway text-[15px] max-sm:text-[13px]'>{poll.createdAt.toDate().toLocaleDateString()}</p>
                                 </span>
                             </div>
                             <Link to={`/polls/${poll.id}`}>
